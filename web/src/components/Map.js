221b77f1b10d52ec5034bf3d06c05e3c0ba71972
Rha02 <romuzus08@gmail.com>
2021-10-16 13:25:09 -0700

    My Merge

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -6,31 +6,22 @@ import { get_scores } from '../helper/lambda';
 class Map extends Component {
     constructor(props) {
         super(props);
-        this.state = {data: this.runInference()};
+        this.state = {
+            data: {},
+            scoresPromise: {}
+        };
     }
 
     async componentDidMount() {
+        console.log("hit component did mount")
         for (var i in counties['features']) {
             let feature = counties['features'][i];
             feature['id'] = feature['properties']['GEO_ID'].substring(9);
         }
-        const data = await this.runInference()
-        console.log(data);
-        this.setState({
-            data: data
-        })
-        console.log(this.state);
-    }
 
-    async runInference() {
-        //make AWS lambda call here 
-        // const inference = {
-        //     'statusCode': 200, 
-        //     'body': {'0': '1', '1': '0.01034', '2': '0.74', '3': '0.3752', '4': '0.02414',
-        //                 '5': '0.5', '6': '0.6', '7': '0.7', '8': '0.8', '9': '0.9'}};
-        
-        await get_scores().then((res) => {
-            const inference = res.Payload
+        const scoresPromise = get_scores().then((res) => {
+            console.log("hit then() of score promise")
+            const inference = JSON.parse(res.Payload)
             // console.log(inference);
             const county_map = {'0': '06085', '1': '06087', '2': '06081', '3': '06075', '4': '06001', '5': '06077',
                                 '6': '06099', '7': '06047', '8': '06069', '9': '06053'};
@@ -44,12 +35,43 @@ class Map extends Component {
                 valList.push(inference["body"][key]);
                 nameList.push(county_names[key]);
             }
-            return {'id': idList, 'val': valList, 'name': nameList};
+            this.setState({ data: {'id': idList, 'val': valList, 'name': nameList}});
+        })
+
+        await scoresPromise
+
+        this.setState({
+            scoresPromise: scoresPromise
         })
     }
+    // async runInference() {
+    //     console.log("hit run inference")
+    //     //make AWS lambda call here 
+    //     // const inference = {
+    //     //     'statusCode': 200, 
+    //     //     'body': {'0': '1', '1': '0.01034', '2': '0.74', '3': '0.3752', '4': '0.02414',
+    //     //                 '5': '0.5', '6': '0.6', '7': '0.7', '8': '0.8', '9': '0.9'}};
+        
+    //     // await get_scores().then((res) => {
+    //     //     const inference = res.Payload
+    //     //     // console.log(inference);
+    //     //     const county_map = {'0': '06085', '1': '06087', '2': '06081', '3': '06075', '4': '06001', '5': '06077',
+    //     //                         '6': '06099', '7': '06047', '8': '06069', '9': '06053'};
+    //     //     const county_names = {'0': 'Santa Clara', '1': 'Santa Cruz', '2': 'San Mateo', '3': 'San Francisco', '4': 'Alameda',
+    //     //                         '5': 'San Joaquin', '6': 'Stanislaus', '7': 'Merced', '8': 'San Benito', '9': 'Monterey'};
+    //     //     var idList = [];
+    //     //     var valList = [];
+    //     //     var nameList = [];
+    //     //     for (var key in inference["body"]) {
+    //     //         idList.push(county_map[key]);
+    //     //         valList.push(inference["body"][key]);
+    //     //         nameList.push(county_names[key]);
+    //     //     }
+    //     //     return {'id': idList, 'val': valList, 'name': nameList};
+    //     // })
+    // }
 
     render() {
-        console.log(this.state);
         return (
             <div>
                 <Plot
